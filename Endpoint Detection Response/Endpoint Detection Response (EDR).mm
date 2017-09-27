f1b7525041cc2c8a8e774c4d13ff14c26830a875
C|DEF {Cyber Defense} Community <32251435+cdefcomm@users.noreply.github.com>
2017-09-27 09:34:11 +0700

    Mindmap files

---
+++ Endpoint Detection Response/Endpoint Detection Response (EDR).mm
@@ -0,0 +1,48 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1506478365780" ID="ID_566553330" MODIFIED="1506478382797" TEXT="Endpoint Detection Response (EDR)">
+<node CREATED="1506478385491" ID="ID_1069565256" MODIFIED="1506478540355" POSITION="right" TEXT="Security Issue of Endpoint">
+<node CREATED="1506478406378" ID="ID_1138406116" MODIFIED="1506478550197" TEXT="Lateral Movement ">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1506478409543" ID="ID_1158014239" MODIFIED="1506478557576" TEXT="Data Exfiltration">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1506478446552" ID="ID_1544012992" MODIFIED="1506478562858" TEXT="Process Monitoring ">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1506478462748" ID="ID_1597402495" MODIFIED="1506478567191" TEXT="Identity Management">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1506478475091" ID="ID_126964976" MODIFIED="1506478572275" TEXT="Patch Management">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1506478489662" ID="ID_986845490" MODIFIED="1506478576488" TEXT="Gaining Access / Privilege Escalation">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1506478498790" ID="ID_1147284219" MODIFIED="1506478580870" TEXT="Device Control">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1506478510593" ID="ID_349158732" MODIFIED="1506478588114" TEXT=" Application Control">
+<icon BUILTIN="full-8"/>
+</node>
+<node CREATED="1506478523069" ID="ID_1171682245" MODIFIED="1506478592585" TEXT="File Sharing Issue">
+<icon BUILTIN="full-9"/>
+</node>
+<node CREATED="1506478535096" ID="ID_1142326405" MODIFIED="1506478598879" TEXT="Administration Control ">
+<icon BUILTIN="full-0"/>
+</node>
+</node>
+<node CREATED="1506478634102" ID="ID_1611362684" MODIFIED="1506478637595" POSITION="left" TEXT="Tautan">
+<node CREATED="1506478638205" ID="ID_526845648" MODIFIED="1506478640396" TEXT="Bacaan">
+<node CREATED="1506478664837" ID="ID_145595203" MODIFIED="1506478710764" TEXT="Lateral Movement, https://attack.mitre.org/wiki/Lateral_Movement"/>
+</node>
+<node CREATED="1506478641702" ID="ID_172801723" MODIFIED="1506478647667" TEXT="Video Cast">
+<node CREATED="1506478681019" ID="ID_148673404" MODIFIED="1506478705442" TEXT="https://www.irongeek.com/i.php?page=videos/derbycon7/t405-hunting-lateral-movement-for-fun-and-profit-mauricio-velazco"/>
+</node>
+<node CREATED="1506478723514" ID="ID_597033285" MODIFIED="1506478731658" TEXT="Group Discussion">
+<node CREATED="1506478732635" ID="ID_200414606" MODIFIED="1506478742844" TEXT="Open Discussion, 22nd September 2017, https://pastebin.com/ytcHP0N4"/>
+</node>
+</node>
+</node>
+</map>
